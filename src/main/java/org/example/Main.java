8ae9128595a6b2b8bfce127af50e7018a1562420
School System <maxelantra@gmail.com>
2025-12-05 08:49:22 -0500

    Update project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int num1 = 12;
-        System.out.println(num1); //prints
+        System.out.println(num1); //prints out
 
 
     }
